15c33cd5b1dfbe843033c429c9ef777620a4921f
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-05-04 11:33:54 +0200

    correct paths in example

--- examples/example-001-basic-read-write.py
+++ examples/example-001-basic-read-write.py
@@ -5,12 +5,11 @@ from exfor_parser import ExforBaseParser
 # reading and writing an EXFOR entry
 # with pointers
 parser = ExforBaseParser()
-exfor_entry = parser.readfile('../testdata/alldata/entry_21308.txt')
+exfor_entry = parser.readfile('../testdata/entry_21308.txt')
 parser.writefile('entry_21308_reproduced.txt', exfor_entry)
-
 # reading and writing an EXFOR entry
 # with COMMON field spanning multiple columns
 parser = ExforBaseParser()
-exfor_entry = parser.readfile('../testdata/alldata/entry_O2098.txt')
+exfor_entry = parser.readfile('../testdata/entry_O2098.txt')
 parser.writefile('entry_O2098_reproduced.txt', exfor_entry)
 
