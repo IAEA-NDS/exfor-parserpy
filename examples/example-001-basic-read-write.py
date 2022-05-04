9e3e4d54a6e9057ca7f280ab296b23fa9af1145a
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-05-04 11:24:38 +0200

    extend example

--- examples/example-001-basic-read-write.py
+++ examples/example-001-basic-read-write.py
@@ -2,7 +2,15 @@ import sys
 sys.path.append('..')
 from exfor_parser import ExforBaseParser
 
+# reading and writing an EXFOR entry
+# with pointers
 parser = ExforBaseParser()
-exfor_entry = parser.readfile('../testdata/alldata/entry_33109.txt')
-parser.writefile('entry_33109_reproduced.txt', exfor_entry)
+exfor_entry = parser.readfile('../testdata/alldata/entry_21308.txt')
+parser.writefile('entry_21308_reproduced.txt', exfor_entry)
+
+# reading and writing an EXFOR entry
+# with COMMON field spanning multiple columns
+parser = ExforBaseParser()
+exfor_entry = parser.readfile('../testdata/alldata/entry_O2098.txt')
+parser.writefile('entry_O2098_reproduced.txt', exfor_entry)
 
