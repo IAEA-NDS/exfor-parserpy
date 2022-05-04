6960bf1424b20184d5e1709265ea6c7f335e36be
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-05-04 21:16:40 +0200

    extend example

--- examples/example-001-basic-read-write.py
+++ examples/example-001-basic-read-write.py
@@ -23,3 +23,10 @@ parser.writefile('entry_O2098_reproduced.txt', exfor_entry, overwrite=True)
 transformed_entry = unitfy(exfor_entry)
 parser.writefile('entry_O2098_transformed.txt', transformed_entry, overwrite=True)
 
+# reading an EXFOR entry, modifying some information
+# and writing it back to a file
+parser = ExforBaseParser()
+exfor_entry = parser.readfile('../testdata/entry_O2098.txt')
+exfor_entry['O2098']['O2098001']['BIB']['AUTHOR'] = 'Some illegal EXFOR here'
+parser.writefile('entry_O2098_modified.txt', exfor_entry, overwrite=True)
+
