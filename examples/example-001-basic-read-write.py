e6f9de9ed1b8dd4510695857be68205d10bb6df6
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-05-14 14:59:38 +0200

    update example

--- examples/example-001-basic-read-write.py
+++ examples/example-001-basic-read-write.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('..')
 from exfor_parserpy import ExforBaseParser
-from exfor_parserpy.trafos import unitfy
+from exfor_parserpy.trafos import unitfy, uncommonfy
 
 # reading and writing an EXFOR entry
 # with pointers
@@ -29,4 +29,24 @@ parser = ExforBaseParser()
 exfor_entry = parser.readfile('../testdata/entry_O2098.txt')
 exfor_entry['O2098']['O2098001']['BIB']['AUTHOR'] = 'Some illegal EXFOR here'
 parser.writefile('entry_O2098_modified.txt', exfor_entry, overwrite=True)
+
+# test the uncommonfy transformer with a common field
+# in the first subentry
+parser = ExforBaseParser()
+exfor_entry = parser.readfile('../testdata/entry_21308.txt')
+transformed_entry = uncommonfy(exfor_entry)
+exfor_entry['21308']['21308002']['DATA']['UNIT']
+transformed_entry['21308']['21308002']['DATA']['UNIT']
+exfor_entry['21308']['21308002']['DATA']['DATA']
+transformed_entry['21308']['21308002']['DATA']['DATA']
+
+# test the uncommonfy transformer with a common field
+# in the subentry with the data
+parser = ExforBaseParser()
+exfor_entry = parser.readfile('../testdata/entry_O2098.txt')
+transformed_entry = uncommonfy(exfor_entry)
+exfor_entry['O2098']['O2098002']['DATA']['UNIT']
+transformed_entry['O2098']['O2098002']['DATA']['UNIT']
+exfor_entry['O2098']['O2098002']['DATA']['DATA']
+transformed_entry['O2098']['O2098002']['DATA']['DATA']
 
