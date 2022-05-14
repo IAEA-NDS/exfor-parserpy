05e889432998f99d8953083dff5fb8d74999324b
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-05-14 12:42:02 +0200

    add empty line

--- exfor_parserpy/utils/convenience.py
+++ exfor_parserpy/utils/convenience.py
@@ -7,6 +7,7 @@
 # License:      MIT
 #
 ############################################################
+
 def apply_factor(data, fact):
     if isinstance(data, list):
         newdata = [d*fact if d is not None else None for d in data]
