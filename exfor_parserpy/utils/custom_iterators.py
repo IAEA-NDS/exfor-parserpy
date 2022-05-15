2411d48599480c55f165120a5830343ed2bbc81a
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-05-15 09:45:26 +0200

    implement exfor iterator with filter callback

--- exfor_parserpy/utils/custom_iterators.py
+++ exfor_parserpy/utils/custom_iterators.py
@@ -26,3 +26,16 @@ def exfor_iterator2(exfor_dic, key=None):
                 yield from exfor_iterator2(item, curkey)
         yield key, exfor_dic
 
+def exfor_iterator3(exfor_dic, key=None, parent=None, filterfun=None):
+    """Traverse all subdictionaries bottom-up."""
+    if is_dic(exfor_dic):
+        # we only recurse into elements of the current
+        # dictionary if the filterfun applied to it returns False
+        if not filterfun or not filterfun(exfor_dic):
+            for curkey, item in exfor_dic.items():
+                if is_dic(item):
+                    yield from exfor_iterator3(item, curkey, exfor_dic, filterfun)
+        # in all cases, we return the current dictionary
+        # even if filterfun evaluates to True
+        yield key, exfor_dic, parent
+
