81385eac1a6e721ecacc743cbac6c56a04d1bf21
Paul Reiter <reiter.paul@gmail.com>
2022-05-29 19:11:14 +0200

    Move testdata to separate tests directory
    This adapts the projects directory structure to the default used by
    poetry.

--- examples/example-001-basic-read-write.py
+++ examples/example-001-basic-read-write.py
@@ -1,12 +1,13 @@
-import sys
-sys.path.append('..')
+from pathlib import Path
 from exfor_parserpy import ExforBaseParser
 from exfor_parserpy.trafos import unitfy, uncommonfy, depointerfy
+
+TEST_DATA = Path(__file__).resolve().parents[1] / "tests" / "testdata"
 
 # reading and writing an EXFOR entry
 # with pointers
 parser = ExforBaseParser()
-exfor_entry = parser.readfile('../testdata/entry_21308.txt')
+exfor_entry = parser.readfile(TEST_DATA / 'entry_21308.txt')
 parser.writefile('entry_21308_reproduced.txt', exfor_entry, overwrite=True)
 
 # transform the units present in the EXFOR entry
@@ -18,7 +19,7 @@ parser.writefile('entry_21308_transformed.txt', transformed_entry, overwrite=Tru
 # reading and writing an EXFOR entry
 # with COMMON field spanning multiple columns
 parser = ExforBaseParser()
-exfor_entry = parser.readfile('../testdata/entry_O2098.txt')
+exfor_entry = parser.readfile(TEST_DATA / 'entry_O2098.txt')
 parser.writefile('entry_O2098_reproduced.txt', exfor_entry, overwrite=True)
 transformed_entry = unitfy(exfor_entry)
 parser.writefile('entry_O2098_transformed.txt', transformed_entry, overwrite=True)
@@ -26,14 +27,14 @@ parser.writefile('entry_O2098_transformed.txt', transformed_entry, overwrite=Tru
 # reading an EXFOR entry, modifying some information
 # and writing it back to a file
 parser = ExforBaseParser()
-exfor_entry = parser.readfile('../testdata/entry_O2098.txt')
+exfor_entry = parser.readfile(TEST_DATA / 'entry_O2098.txt')
 exfor_entry['O2098']['O2098001']['BIB']['AUTHOR'] = 'Some illegal EXFOR here'
 parser.writefile('entry_O2098_modified.txt', exfor_entry, overwrite=True)
 
 # test the uncommonfy transformer with a common field
 # in the first subentry
 parser = ExforBaseParser()
-exfor_entry = parser.readfile('../testdata/entry_21308.txt')
+exfor_entry = parser.readfile(TEST_DATA / 'entry_21308.txt')
 transformed_entry = uncommonfy(exfor_entry)
 exfor_entry['21308']['21308002']['DATA']['UNIT']
 transformed_entry['21308']['21308002']['DATA']['UNIT']
@@ -43,7 +44,7 @@ transformed_entry['21308']['21308002']['DATA']['DATA']
 # test the uncommonfy transformer with a common field
 # in the subentry with the data
 parser = ExforBaseParser()
-exfor_entry = parser.readfile('../testdata/entry_O2098.txt')
+exfor_entry = parser.readfile(TEST_DATA / 'entry_O2098.txt')
 transformed_entry = uncommonfy(exfor_entry)
 exfor_entry['O2098']['O2098002']['DATA']['UNIT']
 transformed_entry['O2098']['O2098002']['DATA']['UNIT']
@@ -53,7 +54,7 @@ transformed_entry['O2098']['O2098002']['DATA']['DATA']
 # test the depointerfy transformer to split up
 # subentries with pointers present
 parser = ExforBaseParser()
-exfor_entry = parser.readfile('../testdata/entry_21308.txt')
+exfor_entry = parser.readfile(TEST_DATA / 'entry_21308.txt')
 transformed_entry = depointerfy(exfor_entry)
 parser.writefile('entry_21308_depointered.txt', transformed_entry, overwrite=True)
 
