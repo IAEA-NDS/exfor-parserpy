26135f11664ac7174a379d8f5dc2017e8ee1db54
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-06-20 19:38:25 +0200

    introduce exforfile option for unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,15 @@ from pathlib import Path
 
 def pytest_addoption(parser):
     parser.addoption("--exfordir", action="store", default="testdata")
+    parser.addoption("--exforfile", action="store", default=None)
 
 
 def pytest_generate_tests(metafunc):
     exfor_dir = Path(__file__).parent / metafunc.config.option.exfordir
     if "entry_file" in metafunc.fixturenames:
-        metafunc.parametrize("entry_file", exfor_dir.glob("*.txt"))
+        file_opt = metafunc.config.option.exforfile
+        if file_opt is not None:
+            entry_files = [exfor_dir / file_opt]
+        else:
+            entry_files = exfor_dir.glob("*.txt")
+        metafunc.parametrize("entry_file", entry_files)
