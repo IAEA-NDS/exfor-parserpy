27695443e78376483cb9af65a22fd3e48ee2df78
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-06-20 19:39:32 +0200

    fix wrong variable in assertion messages

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -9,7 +9,7 @@ def test_unitfy_never_fails(entry_file):
     try:
         unitfy(content)
     except Exception as exc:
-        assert False, f"unitfy failed on file {file} with exception {exc}"
+        assert False, f"unitfy failed on file {entry_file} with exception {exc}"
 
 
 def test_depointerfy_never_fails(entry_file):
@@ -17,7 +17,7 @@ def test_depointerfy_never_fails(entry_file):
     try:
         depointerfy(content)
     except Exception as exc:
-        assert False, f"depointerfy failed on file {file} with exception {exc}"
+        assert False, f"depointerfy failed on file {entry_file} with exception {exc}"
 
 
 def test_detextify_never_fails(entry_file):
@@ -25,7 +25,7 @@ def test_detextify_never_fails(entry_file):
     try:
         detextify(content)
     except Exception as exc:
-        assert False, f"detextify failed on file {file} with exception {exc}"
+        assert False, f"detextify failed on file {entry_file} with exception {exc}"
 
 
 def test_uncommonfy_never_fails(entry_file):
@@ -33,4 +33,4 @@ def test_uncommonfy_never_fails(entry_file):
     try:
         uncommonfy(content)
     except Exception as exc:
-        assert False, f"uncommonfy failed on file {file} with exception {exc}"
+        assert False, f"uncommonfy failed on file {entry_file} with exception {exc}"
