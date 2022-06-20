6c959a5212f044272f6093069a60ba9d6bc462ca
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-06-20 14:18:24 +0200

    update unit test structure for expression_parsing

--- tests/test_expression_parsing.py
+++ tests/test_expression_parsing.py
@@ -2,7 +2,7 @@ import pytest
 from exfor_parserpy.utils.arithmetic_expr_parsing_new import parse_arithm_expr
 
 
-def parse_usersym(expr, ofs):
+def parse_number_str(expr, ofs):
     startofs = ofs
     while ofs < len(expr) and (expr[ofs].isdigit()):
         ofs += 1
@@ -12,12 +12,19 @@ def parse_usersym(expr, ofs):
     return node, ofs
 
 
-def eval_expr_tree(expr_tree):
+def eval_number_node(expr_tree):
+    if expr_tree["type"] == "number":
+        return expr_tree["number"]
+    else:
+        raise TypeError("Invalid node type")
+
+
+def eval_expr_tree(expr_tree, eval_fun):
     # binary operators
     ntyp = expr_tree["type"]
     if ntyp in ("add", "sub", "prod", "div"):
-        leftval = eval_expr_tree(expr_tree["children"][0])
-        rightval = eval_expr_tree(expr_tree["children"][1])
+        leftval = eval_expr_tree(expr_tree["children"][0], eval_fun)
+        rightval = eval_expr_tree(expr_tree["children"][1], eval_fun)
         if ntyp == "add":
             return leftval + rightval
         elif ntyp == "sub":
@@ -28,14 +35,14 @@ def eval_expr_tree(expr_tree):
             return leftval / rightval
     # unary operators
     if ntyp in ("neg", "bracket"):
-        val = eval_expr_tree(expr_tree["children"][0])
+        val = eval_expr_tree(expr_tree["children"][0], eval_fun)
         if ntyp == "neg":
             return -val
         elif ntyp == "bracket":
             return val
-    # a value
-    if ntyp == "number":
-        return expr_tree["number"]
+    # not known how to evaluate the node so
+    # this must be done by the user_eval_fun
+    return eval_fun(expr_tree)
 
 
 @pytest.mark.parametrize(
@@ -51,8 +58,8 @@ def eval_expr_tree(expr_tree):
     ),
 )
 def test_arithmetic_expression_parsing_works_correctly(arithm_expr_str, expect_res):
-    tree, _ = parse_arithm_expr(arithm_expr_str, parse_usersym)
-    res = eval_expr_tree(tree)
+    tree, _ = parse_arithm_expr(arithm_expr_str, parse_number_str)
+    res = eval_expr_tree(tree, eval_number_node)
     assert (
         res == expect_res
     ), f"wrong result {res} for {arithm_expr_str}, expected {expect_res}"
