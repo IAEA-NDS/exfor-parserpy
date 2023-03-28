520e73fcee43dcaadabf24ad29b2ffc64703f861
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-03-28 13:49:43 +0200

    more improvements to diff regarding COMMON and DATA blocks

--- exfor_parserpy/exfor_diff.py
+++ exfor_parserpy/exfor_diff.py
@@ -32,16 +32,14 @@ import numpy as np
 import html
 
 
-def compute_edit_matrix(str1, str2):
+def compute_edit_matrix(str1, str2, cost_sub=100):
     m = np.zeros((len(str1) + 1, len(str2) + 1), dtype=float)
     m[:, 0] = np.arange(m.shape[0])
     m[0, :] = np.arange(m.shape[1])
     for j in range(len(str2)):
         for i in range(len(str1)):
-            cost_sub = 0.0 if str1[i] == str2[j] else 100.0
-            m[i + 1, j + 1] = min(
-                m[i, j + 1] + 1.0, m[i + 1, j] + 1.0, m[i, j] + cost_sub
-            )
+            cost = 0.0 if str1[i] == str2[j] else cost_sub
+            m[i + 1, j + 1] = min(m[i, j + 1] + 1.0, m[i + 1, j] + 1.0, m[i, j] + cost)
     return m
 
 
@@ -76,6 +74,20 @@ def compute_edit_path(str1, str2):
     return np.array(p)
 
 
+def postprocess_edit_type(edit_type, string):
+    edit_type = edit_type.copy()
+    for i in range(len(edit_type) - 1, 0, -1):
+        if edit_type[i] != "i":
+            continue
+        j = i - 1
+        while j >= 0 and edit_type[j] == "i":
+            j -= 1
+        if j >= 0 and string[i] == string[j]:
+            edit_type[i] = "k"
+            edit_type[j] = "i"
+    return edit_type
+
+
 def find_last_nonwhite(string):
     i = len(string) - 1
     while i >= 0 and string[i] == " ":
@@ -91,7 +103,30 @@ def enhanced_escape(string, escape=True):
     return newstring
 
 
-def add_diff_markers(
+def add_tablecell_diff_markers(
+    lines, mark_mask, start_marker="<mark>", end_marker="</mark>", escape=True
+):
+    s = "".join(lines)
+    fields = [s[i : i + 11] for i in range(0, len(s), 11)]
+    lines = []
+    curline = ""
+    for i, m in enumerate(mark_mask):
+        field = fields[i]
+        curstr = enhanced_escape(field.rjust(11), escape)
+        if m:
+            curstr = start_marker + curstr + end_marker
+        curline += curstr
+        if i % 6 == 5:
+            lines.append(curline.ljust(66))
+            curline = ""
+    need_num = len(mark_mask) % 6
+    if need_num != 0:
+        curline += enhanced_escape(" " * (11 * (6 - need_num)), escape)
+        lines.append(curline)
+    return lines
+
+
+def add_bibfield_diff_markers(
     edit_type_arr,
     string,
     edit_type,
@@ -129,21 +164,30 @@ def add_diff_markers(
         return newstr
 
 
-def align_side_by_side(lines1=None, lines2=None, escape=True):
+def align_side_by_side(
+    lines1=None,
+    lines2=None,
+    escape=True,
+    escape_blank=True,
+    noline_start_marker='<mark class="noline">',
+    noline_end_marker="</mark>",
+):
     if isinstance(lines1, str):
         lines1 = [lines1]
     if isinstance(lines2, str):
         lines2 = [lines2]
     lines1 = lines1.copy() if lines1 is not None else []
     lines2 = lines2.copy() if lines2 is not None else []
-    lendiff = len(lines2) - len(lines1)
-    if lendiff > 0:
-        lines1.extend([" " * 66] * lendiff)
-    elif lendiff < 0:
-        lines2.extend([" " * 66] * lendiff)
     if escape:
         lines1 = [enhanced_escape(line) for line in lines1]
         lines2 = [enhanced_escape(line) for line in lines2]
+    lendiff = len(lines2) - len(lines1)
+    blankline = " " * 66 if not escape_blank else "&nbsp;" * 66
+    blankline = noline_start_marker + blankline + noline_end_marker
+    if lendiff > 0:
+        lines1.extend([blankline] * lendiff)
+    elif lendiff < 0:
+        lines2.extend([blankline] * (-lendiff))
     return [l1 + " | " + l2 for l1, l2 in zip(lines1, lines2)]
 
 
@@ -201,11 +245,12 @@ def bib_element_diff(datadic1, datadic2):
             edit_path = compute_edit_path(lc1j, lc2j)
             ep1 = edit_path[np.isin(edit_path, ("d", "k"))]
             ep2 = edit_path[np.isin(edit_path, ("i", "k"))]
+            ep2 = postprocess_edit_type(ep2, lc2j)
             ep1s = [ep1[i:j] for i, j in zip(breaks1[:-1], breaks1[1:])]
             ep2s = [ep2[i:j] for i, j in zip(breaks2[:-1], breaks2[1:])]
             new_lc1 = []
             for e, line in zip(ep1s, lc1):
-                newline = add_diff_markers(
+                newline = add_bibfield_diff_markers(
                     e,
                     line,
                     "d",
@@ -217,7 +262,7 @@ def bib_element_diff(datadic1, datadic2):
 
             new_lc2 = []
             for e, line in zip(ep2s, lc2):
-                newline = add_diff_markers(
+                newline = add_bibfield_diff_markers(
                     e,
                     line,
                     "i",
@@ -227,7 +272,9 @@ def bib_element_diff(datadic1, datadic2):
                 new_lc2.append(newline)
             lines2 = [lh + lc for lh, lc in zip(lh2, new_lc2)]
             # end diff magic
-            curlines = align_side_by_side(lines1, lines2, escape=False)
+            curlines = align_side_by_side(
+                lines1, lines2, escape=False, escape_blank=True
+            )
             first1 = False
             first2 = False
         lines.extend(curlines)
@@ -288,6 +335,8 @@ def common_or_data_diff(datadic1, datadic2, what="common"):
         k: {" ": v} if not contains_pointers(v) else v for k, v in unit_dic2.items()
     }
 
+    remove_mask = []
+    insert_mask = []
     fieldkeys = sorted(set(unit_dic1).union(unit_dic2))
     for fieldkey in fieldkeys:
         if fieldkey not in unit_dic2:
@@ -295,37 +344,67 @@ def common_or_data_diff(datadic1, datadic2, what="common"):
             for pointer in cont1:
                 descrs1.append((fieldkey, pointer))
                 units1.append(cont1[pointer])
+                remove_mask.append(True)
         elif fieldkey not in unit_dic1:
             cont2 = unit_dic2[fieldkey]
             for pointer in cont2:
                 descrs2.append((fieldkey, pointer))
-                units2.append(cont1[pointer])
+                units2.append(cont2[pointer])
+                insert_mask.append(True)
         else:
             cont1 = unit_dic1[fieldkey]
             cont2 = unit_dic2[fieldkey]
             pointers = sorted(set(unit_dic1[fieldkey]).union(unit_dic2[fieldkey]))
             for pointer in pointers:
-                if pointer not in unit_dic2:
+                if pointer not in cont2:
                     descrs1.append((fieldkey, pointer))
                     units1.append(cont1[pointer])
-                elif pointer not in unit_dic1:
+                    remove_mask.append(True)
+                elif pointer not in cont1:
                     descrs2.append((fieldkey, pointer))
-                    units2.append(cont1[pointer])
+                    units2.append(cont2[pointer])
+                    insert_mask.append(True)
                 else:
                     descrs1.append((fieldkey, pointer))
                     units1.append(cont1[pointer])
                     descrs2.append((fieldkey, pointer))
-                    units2.append(cont1[pointer])
+                    units2.append(cont2[pointer])
+                    remove_mask.append(False)
+                    insert_mask.append(False)
 
     # write out the header
     curlines1 = write_fields(descrs1, 0, dtype="strp")
+    curlines1 = add_tablecell_diff_markers(
+        curlines1,
+        remove_mask,
+        start_marker='<mark class="deletion">',
+        end_marker="</mark>",
+    )
     curlines2 = write_fields(descrs2, 0, dtype="strp")
-    curlines = align_side_by_side(curlines1, curlines2)
+    curlines2 = add_tablecell_diff_markers(
+        curlines2,
+        insert_mask,
+        start_marker='<mark class="addition">',
+        end_marker="</mark>",
+    )
+    curlines = align_side_by_side(curlines1, curlines2, escape=False)
     lines.extend(curlines)
 
     curlines1 = write_fields(units1, 0, dtype="str")
+    curlines1 = add_tablecell_diff_markers(
+        curlines1,
+        remove_mask,
+        start_marker='<mark class="deletion">',
+        end_marker="</mark>",
+    )
     curlines2 = write_fields(units2, 0, dtype="str")
-    curlines = align_side_by_side(curlines1, curlines2)
+    curlines2 = add_tablecell_diff_markers(
+        curlines2,
+        insert_mask,
+        start_marker='<mark class="addition">',
+        end_marker="</mark>",
+    )
+    curlines = align_side_by_side(curlines1, curlines2, escape=False)
     lines.extend(curlines)
 
     # write the data
@@ -363,8 +442,20 @@ def common_or_data_diff(datadic1, datadic2, what="common"):
                         values2.append(cont2[pointer])
 
         curlines1 = write_fields(values1, 0, dtype="float")
+        curlines1 = add_tablecell_diff_markers(
+            curlines1,
+            remove_mask,
+            start_marker='<mark class="deletion">',
+            end_marker="</mark>",
+        )
         curlines2 = write_fields(values2, 0, dtype="float")
-        curlines = align_side_by_side(curlines1, curlines2)
+        curlines2 = add_tablecell_diff_markers(
+            curlines2,
+            insert_mask,
+            start_marker='<mark class="addition">',
+            end_marker="</mark>",
+        )
+        curlines = align_side_by_side(curlines1, curlines2, escape=False)
         lines.extend(curlines)
     elif what == "data":
         # get a column of the DATA section table
@@ -388,7 +479,14 @@ def common_or_data_diff(datadic1, datadic2, what="common"):
             for fieldkey, cont in unit_dic1.items():
                 for pointer in cont:
                     values1.append(data_dic1[fieldkey][pointer][currow1])
-            curlines1.extend(write_fields(values1, 0, dtype="float"))
+            curline1 = write_fields(values1, 0, dtype="float")
+            curline1 = add_tablecell_diff_markers(
+                curline1,
+                remove_mask,
+                start_marker='<mark class="deletion">',
+                end_marker="</mark>",
+            )
+            curlines1.extend(curline1)
 
         curlines2 = []
         for currow2 in range(numlines2):
@@ -396,9 +494,16 @@ def common_or_data_diff(datadic1, datadic2, what="common"):
             for fieldkey, cont in unit_dic2.items():
                 for pointer in cont:
                     values2.append(data_dic2[fieldkey][pointer][currow2])
-            curlines2.extend(write_fields(values2, 0, dtype="float"))
+            curline2 = write_fields(values2, 0, dtype="float")
+            curline2 = add_tablecell_diff_markers(
+                curline2,
+                insert_mask,
+                start_marker='<mark class="addition">',
+                end_marker="</mark>",
+            )
+            curlines2.extend(curline2)
 
-        curlines = align_side_by_side(curlines1, curlines2)
+        curlines = align_side_by_side(curlines1, curlines2, escape=False)
         lines.extend(curlines)
 
     end_line = write_str_field("", 0, "ENDCOMMON" if what == "common" else "ENDDATA")
@@ -493,6 +598,7 @@ def output_diff(datadic1, datadic2):
     body { font-family: 'Courier New', monospace; }
     .addition { background-color:#c0ffc8; }
     .deletion { background-color:#ffa07a; }
+    .noline { background-color:#ffffe0; }
     </style></head><body>
     """
     ]
